62853807adc56022f80e972d41ff3a9fbefda05d
Micheal Bailey <mbaileyfuturist@gmail.com>
2022-03-26 22:23:03 -0400

    Displaying pokemon API information onto the pokemon-selector component.

--- src/app/components/pokemon-selector/pokemon-selector.component.ts
+++ src/app/components/pokemon-selector/pokemon-selector.component.ts
@@ -14,9 +14,9 @@ import { map } from 'rxjs/operators';
 })
 export class PokemonSelectorComponent implements OnInit {
 
-  pokemonArray = []
   pokemonAPI = []
   pokemonDataObjects = []
+
 
   constructor(private http: HttpClient) { }
 
@@ -27,12 +27,17 @@ export class PokemonSelectorComponent implements OnInit {
 
   private fetchPokemon(){
 
-    let name = ''
-    let order = ''
-    let characterSelectionSprite = ''
-    let showcaseSprite = ''
     let fourMoves = []
-    //let pokemon = {}
+    let pokemon = {
+      name: '',
+      order: '',
+      characterSelectionSprite:'',
+      showcaseSprite:'',
+      moveOne:'',
+      moveTwo:'',
+      moveThree:'',
+      moveFour:'',
+    }
 
     this.http.get('https://pokeapi.co/api/v2/pokemon?limit=200&offset=0')
     .pipe(map(responseData => {
@@ -42,75 +47,75 @@ export class PokemonSelectorComponent implements OnInit {
         postArray.push(responseData[key])
       }
 
-      this.pokemonArray = postArray[3]
+      const pokemonArray = postArray[3]
 
-      //This will forward the array to our subscribe function
-      return this.pokemonArray
-    }))
-    .subscribe(pokemonArray => {
       for(let key in pokemonArray){
         this.pokemonAPI.push(pokemonArray[key].url)
       }
 
-      //iterate through each pokemon API and get the properties.
-      for(let key in this.pokemonAPI){
-        this.http.get(this.pokemonAPI[key])
-        .subscribe(pokemonData => {
+    //iterate through each pokemon API and get the properties.
+    for(let key in this.pokemonAPI){
+      this.http.get(this.pokemonAPI[key])
+      .subscribe(pokemonData => {
 
-            for(let key in pokemonData){
-              if(key == 'name'){
-                console.log('name: ', pokemonData[key])
-                name = pokemonData[key]
-              }if(key == 'order'){
-                console.log('order: ', pokemonData[key])
-                order = pokemonData[key]
-              }
-              if(key == 'sprites'){
-                for(let spriteKey in pokemonData[key]){
-                  if(spriteKey == 'front_default'){
-                    console.log('Character Selection Sprite URL: ', pokemonData[key][spriteKey])
-                    characterSelectionSprite = pokemonData[key][spriteKey]
-                  }
-                  if(spriteKey == 'other'){
-                      for(let otherKey in pokemonData[key][spriteKey]){
-                        if(otherKey == 'official-artwork'){
-                          console.log('Official-artwork: ', pokemonData[key][spriteKey][otherKey])
-                          showcaseSprite = pokemonData[key][spriteKey][otherKey]
-                        }
-                      }
+          for(let key in pokemonData){
+        if(key == 'name'){
+          //console.log('name: ', pokemonData[key])
+          pokemon.name = pokemonData[key]
+        }if(key == 'order'){
+          //console.log('order: ', pokemonData[key])
+          pokemon.order = pokemonData[key]
+        }
+        if(key == 'sprites'){
+          for(let spriteKey in pokemonData[key]){
+            if(spriteKey == 'front_default'){
+              //console.log('Character Selection Sprite URL: ', pokemonData[key][spriteKey])
+              pokemon.characterSelectionSprite = pokemonData[key][spriteKey]
+            }
+            if(spriteKey == 'other'){
+                for(let otherKey in pokemonData[key][spriteKey]){
+                  if(otherKey == 'official-artwork'){
+                    //console.log('Official-artwork: ', pokemonData[key][spriteKey][otherKey])
+                    pokemon.showcaseSprite = pokemonData[key][spriteKey][otherKey]
                   }
                 }
-              }
-
-              if(key == 'moves'){
-                 let numberOfMoves = 1
-                 for(let index = 0; index < pokemonData[key].length; index++){
-                   let moveData = pokemonData[key][index]
-                    for(let key in moveData){
-                    if(key == 'move' && numberOfMoves <= 4){
-                      fourMoves.push(moveData[key].name)
-                      console.log(moveData[key].name)
-                      numberOfMoves++
-                    }
-                  }
-                 }
-              }
-
-              // pokemon = {
-              //   name: name,
-              //   order: order,
-              //   characterSelectionSprite: characterSelectionSprite,
-              //   showcaseSprite:showcaseSprite,
-              //   moveOne:fourMoves[0],
-              //   moveTwo:fourMoves[1],
-              //   moveThree:fourMoves[2],
-              //   moveFour:fourMoves[3],
-              // }
             }
-        })
+          }
+        }
+
+        if(key == 'moves'){
+           let numberOfMoves = 1
+           for(let index = 0; index < pokemonData[key].length; index++){
+             let moveData = pokemonData[key][index]
+              for(let key in moveData){
+              if(key == 'move' && numberOfMoves <= 4){
+                fourMoves.push(moveData[key].name)
+                //console.log(moveData[key].name)
+                numberOfMoves++
+              }
+              pokemon.moveOne = fourMoves[0]
+              pokemon.moveTwo = fourMoves[1]
+              pokemon.moveThree = fourMoves[2]
+              pokemon.moveFour = fourMoves[3]
+
+            }
+           }
+        }
       }
 
+       console.log(pokemon)
 
+       //We need Object.assign({}, object) to properly append objects to the array.
+       //Otherwise it will not work properly. The previous object will always be overwritten
+       //by the last inevidably returning a list that only contains the laste element in the array.
+      this.pokemonDataObjects.push(Object.assign({}, pokemon))
+
+      })
+    }
+      return this.pokemonDataObjects
+    }))
+    .subscribe(pokemonObjects => {
+      console.log(pokemonObjects)
     })
 
   }
